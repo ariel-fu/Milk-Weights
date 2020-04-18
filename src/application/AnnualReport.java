8898ff28fb60c0736c3c83802fa7b3d88343f932
PrasunGuragain <61520252+PrasunGuragain@users.noreply.github.com>
2020-04-17 22:51:58 -0500

    Revert "did some gui"
    This reverts commit 817bf353a26ad9e1924fd583197c236ada179d15.

---
+++ src/application/AnnualReport.java
@@ -0,0 +1,10 @@
+package application;
+
+/**
+ * This class models an Annual Report requested by the user
+ * @author Alex, Ariel, Catherine, Harry, Prasun
+ *
+ */
+public class AnnualReport extends ReportBase {
+
+}
