3787b51e386bdf65a59042fee0dad534a9a535f2
ariel-fu <60861741+ariel-fu@users.noreply.github.com>
2020-04-25 18:39:12 -0500

    Update ReportBase.java

--- ReportBase.java
+++ ReportBase.java
@@ -1,3 +1,4 @@
+
 package application;
 
 import java.util.List;
@@ -11,20 +12,52 @@ import java.util.List;
  */
 public abstract class ReportBase {
 
-  
-  abstract List<Double> getPercents();
+  /**
+   * This method calculate how much percent does each month contributes towards
+   * the entire year
+   * 
+   * @return an arrayList of percentages of each month
+   */
+  List<Double> getPercents() {
+    return this.percents;
+  }
 
-  
-  abstract double getAvg();
-  
-  abstract double getMin();
-  
-  abstract double getMax();
-  
-  abstract boolean validateInputs();
-  
-  
-  
-  
-  
+  List<String> getPercentLabels() {
+    return this.percentLabels;
+  }
+
+  /**
+   * This method gets the overall average milk weight for the whole year
+   * 
+   * @return the average weight of milk for the whole year
+   */
+  double getAvg() {
+    return this.average;
+  }
+
+  /**
+   * This method returns the month with the lowest yield of milk
+   */
+  double getMin() {
+    return this.min;
+  }
+
+  /**
+   * This method returns the month with the most yield of milk
+   * 
+   * @return the month that had the most yield of milk
+   **/
+  double getMax() {
+    return this.max;
+  }
+
+  protected double average;
+  protected double min;
+  protected double max;
+  protected List<Double> percents;
+  protected List<String> percentLabels;
+
+  // abstract class
+  public abstract void runReport();
 }
+
